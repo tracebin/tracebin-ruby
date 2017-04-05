cd1b88c5bf4fcca05fd4213696a59825a45f882c
Konstantin M <sub@linuxpath.com>
2017-04-05 11:58:00 -0700

    Events subclasses New data storage format

--- lib/vizsla/subscribers.rb
+++ lib/vizsla/subscribers.rb
@@ -1,6 +1,70 @@
 require 'vizsla/recorder' unless defined?(::Vizsla::Recorder)
 
 module Vizsla
+  class Event
+    attr_reader :event
+
+    def initialize(event)
+      @event = event
+    end
+
+    def recorder_type
+      event[0]
+    end
+
+    def valid?
+      true
+    end
+
+    def prettify_data
+      {
+        event_started: event[1],
+        event_ended: event[2],
+        event_duration: event[2] - event[1],
+        event_payload: prettify_payload
+      }
+    end
+  end
+
+  class SQLEvent < Event
+    def valid?
+      event.last[:name] != "SCHEMA"
+    end
+
+    private
+
+    def prettify_payload
+      {
+        query: event.last[:sql]
+      }
+    end
+  end
+
+  class ControllerEvent < Event
+    private
+
+    def prettify_payload
+      payload = event.last
+      {
+        format: payload[:format],
+        controller: payload[:controller],
+        action: payload[:action],
+        path: payload[:path],
+        db_runtime: payload[:db_runtime]
+      }
+    end
+  end
+
+  class ViewEvent < Event
+    private
+
+    def prettify_payload
+      {
+        layout: event.last[:layout]
+      }
+    end
+  end
+
   class Subscribers
     def initialize
       @events_data = Recorder
@@ -10,25 +74,22 @@ module Vizsla
 
     def sql_hook
       ActiveSupport::Notifications.subscribe "sql.active_record" do |*args|
-        sql_data = args.extract_options!
-        @events_data << sql_data[:sql] unless sql_data[:name] == "SCHEMA"
+        event = SQLEvent.new(args)
+        @events_data << event if event.valid?
       end
     end
 
     def process_action_hook
       ActiveSupport::Notifications.subscribe "process_action.action_controller" do |*args|
-        controller_data = args.extract_options!
-        @events_data << {
-          responded_controller: controller_data[:controller],
-          requested_path: controller_data[:path]
-        }
+        event = ControllerEvent.new(args)
+        @events_data << event
       end
     end
 
     def render_template_hook
       ActiveSupport::Notifications.subscribe "render_template.action_view" do |*args|
-        template_data = args.extract_options!
-        @events_data << { rendered_layout: template_data[:layout] }
+        event = ViewEvent.new(args)
+        @events_data << event
       end
     end
 
